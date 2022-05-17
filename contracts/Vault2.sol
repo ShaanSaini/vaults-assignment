b11bd694fee88a0c76b98fba7aaea84ef4f8ab27
ShaanSaini <roarosaur@Shaans-MacBook-Pro.local>
2022-05-17 13:17:17 -0700

    Test commit

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -2,6 +2,24 @@
 
 pragma solidity ^0.8.4;
 
-contract Vault2 {
+contract Vault2 is ERC20, Ownable {
     
+    // Events
+    event Mint(uint amount);
+    event Burn(uint amount);
+
+    constructor() ERC20("Vault2", "VAULT") {}
+
+    function mint(_amount) external payable onlyOwner {
+        require(_amount == msg.value, "amount does not equal to msg.value");
+        _mint(msg.sender, _amount);
+        emit Mint(_amount);
+    }
+
+    function burn(_amount) external payable onlyOwner {
+        _burn(msg.sender, _amount);
+        (bool _success,) = msg.sender.call{value: _amount}("");
+        require(success, "Withdraw failed");
+        emit Burn(_amount);
+    }
 }
